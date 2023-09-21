e712d6df329c11768a9161002468a4e164ba4705
Garakoti <rahulsinghgarakoti730@gmail.com>
2023-09-21 12:31:16 +0530

    Adding the source code

---
+++ app.js
@@ -0,0 +1,19 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  console.log(voices);
+  speech.voice = voices[0];
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
+};
+
+document.querySelector("button").addEventListener("click", () => {
+  let text = document.querySelector("textarea").value;
+  speech.text = text;
+  window.speechSynthesis.speak(speech);
+});
